21bd730e43d7fbac7d0c2f9a96d35f812a4f1223
Ameer <ameer.abdulsalam@fineway.de>
2018-06-04 23:57:29 +0200

    Completed chapter 3

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Rango says hey there partner!')
+    return HttpResponse('Rango says hey there partner! <br/> <a href=\'/rango/about/\'>About</a>')
+
+def about(request):
+    return HttpResponse('About page <br/> <a href=\'/rango/\'>Index</a>')
